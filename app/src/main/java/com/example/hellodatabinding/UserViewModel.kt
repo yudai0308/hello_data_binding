691503c3b45b71331f2859fde90314fdb3412de2
Yudai Watanabe <lock0308@gmail.com>
2020-05-31 16:22:34 +0900

    コメント修正。

--- app/src/main/java/com/example/hellodatabinding/UserViewModel.kt
+++ app/src/main/java/com/example/hellodatabinding/UserViewModel.kt
@@ -6,9 +6,8 @@ import androidx.lifecycle.ViewModel
 
 class UserViewModel : ViewModel() {
     /**
-     * ここでもし User モデルではなく、単に String 型の LiveData を使ってしまうと
+     * ここで User モデルではなく、単に String 型の LiveData を使ってしまうと
      * 双方向データバインディングが実現できなくなるので注意。
-     * User モデルを実装し、getName と setName を実装する必要がある。
      */
     private val _user = MutableLiveData(User("John Doe"))
     private val _greeting = MutableLiveData("Hello, ${_user.value?.name}!")
@@ -22,4 +21,7 @@ class UserViewModel : ViewModel() {
 
 }
 
+/**
+ * User の name を双方向にバインドしたいので、getName と setName を実装する。
+ */
 class User(var name: String)

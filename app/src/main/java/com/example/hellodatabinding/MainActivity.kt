fe77da544adb6d0901bcf38e470d27553d22bc6d
Yudai Watanabe <lock0308@gmail.com>
2020-05-31 12:03:07 +0900

    一方向から双方向のデータバインディングに変更。

--- app/src/main/java/com/example/hellodatabinding/MainActivity.kt
+++ app/src/main/java/com/example/hellodatabinding/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.hellodatabinding
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.hellodatabinding.databinding.ActivityMainBinding
@@ -17,14 +16,23 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.viewModel = userViewModel
-        binding.lifecycleOwner = this
-        binding.editText.addTextChangedListener { text ->
-            userViewModel.setName(text.toString())
+        /**
+         * binding に lifecycleOwner を設定すれば双方向データバインディングが実現できる。
+         * EditText の入力値が変更されたときの処理や、greeting の値が変更されたときの処理を実装する必要がない。
+         */
+        binding.apply {
+            viewModel = userViewModel
+            lifecycleOwner = this@MainActivity
         }
 
-//        // binding に lifecycleOwner を設定しない場合は一方向データバインディングとなり、
-//        // 以下のようにデータが変更されたときの処理を書かないといけない。
+        /**
+         * 双方向データバインディングでは以下のような処理が必要ない。
+         */
+
+//        binding.editText.addTextChangedListener { text ->
+//            userViewModel.setName(text.toString())
+//        }
+
 //        userViewModel.greeting.observe(this, Observer { greeting ->
 //            binding.textView.text = greeting
 //        })
--- app/src/main/java/com/example/hellodatabinding/UserViewModel.kt
+++ app/src/main/java/com/example/hellodatabinding/UserViewModel.kt
@@ -5,19 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class UserViewModel : ViewModel() {
+    /**
+     * ここでもし User モデルではなく、単に String 型の LiveData を使ってしまうと
+     * 双方向データバインディングが実現できなくなるので注意。
+     * User モデルを実装し、getName と setName を実装する必要がある。
+     */
+    private val _user = MutableLiveData(User("John Doe"))
+    private val _greeting = MutableLiveData("Hello, ${_user.value?.name}!")
 
-    private val _name = MutableLiveData("John Doe")
-    private val _greeting = MutableLiveData("Hello, ${_name.value}!")
-
-    val name: LiveData<String> = _name
+    val user: LiveData<User> = _user
     val greeting: LiveData<String> = _greeting
 
-    fun setName(name: String) {
-        _name.value = name
-    }
-
     fun onClick() {
-        _greeting.value = "Hello, ${name.value}!"
+        _greeting.value = "Hello, ${user.value?.name}!"
     }
 
 }
+
+class User(var name: String)
